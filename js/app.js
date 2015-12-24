cc7842b6ac4f5f511c83fb12e744a8c2ad950af0
Paul-Kijtapart <scc385@gmail.com>
2015-12-24 13:13:32 -0800

    integrated in angular Router

--- js/app.js
+++ js/app.js
@@ -1,2 +1,25 @@
-angular.module('aornawat', ['contact','education','profile','projects','skills'])
+angular.module('aornawat', ['ngRoute', 'contact', 'education', 'profile', 'projects', 'skills'])
 
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+    // route for the home page
+        .when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'myProjects'
+    })
+
+    // route for the about page
+    .when('/androids', {
+        templateUrl: 'templates/androids.html',
+        controller: 'myProjects'
+    })
+
+    // route for the contact page
+    .when('/webs', {
+        templateUrl: 'templates/webs.html',
+        controller: 'myProjects'
+    })
+    .otherwise({
+    	redirectTo: '/'
+    });
+}])
--- js/projects.js
+++ js/projects.js
@@ -1,5 +1,17 @@
 angular.module('projects', [])
 
-.controller('myProjects', ['$scope', function($scope) {
+.controller('myProjects', ['$scope','$http', function($scope, $http) {
+    $http.get('projects/android.json').success(function(data){
+    	$scope.androids = data;
+    });
+
+    $http.get('projects/web.json').success(function(data){
+    	$scope.webs = data;
+    });
+
+
+    $scope.messageA = "android",
+    $scope.messageW = "web",
+    $scope.messageH = "home";
     $scope.test = "projects";
 }]);
