218d8eda60a8d2c7a99167d6b115ab27daec663c
Paul-Kijtapart <scc385@gmail.com>
2015-12-24 18:46:49 -0800

    set up skillEducation section

--- js/skills.js
+++ js/skills.js
@@ -1,9 +1,19 @@
 angular.module('skills', [])
 
-.controller('mySkills', ['$scope', function($scope) {
-	$scope.teamTool = ["Git"];
-	$scope.mobileDev = ["Android"]
-	$scope.webDev = ["HTML&CSS", "Three.js", "angularJS"];
-	$scope.analyticSkill = ["matlab", "R studio"];
-	$scope.databaseSkill = ["SQL"]
+.controller('mySkills', ['$scope', '$http', function($scope, $http) {
+    $scope.teamTool = ["Git"];
+    $scope.mobileDev = ["Android"]
+    $scope.webDev = ["HTML&CSS", "Three.js", "angularJS"];
+    $scope.analyticSkill = ["matlab", "R studio"];
+    $scope.databaseSkill = ["SQL"]
+
+    $http.get('projects/skills.json').success(function(data) {
+        $scope.skills = data;
+    });
+
+    $scope.checkValue = function(skills) {
+        for (var name in skills) {
+            console.log(name);
+        }
+    }
 }]);
