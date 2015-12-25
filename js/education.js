5658cc2339be6ec8d6cdab40200c973984c1e138
Paul-Kijtapart <scc385@gmail.com>
2015-12-24 18:15:04 -0800

    updated education

--- js/education.js
+++ js/education.js
@@ -1,5 +1,7 @@
 angular.module('education', [])
 
 .controller('myEducation', ['$scope', function($scope) {
+    $scope.coursesCompleted = ["CPSC340 - Machine Learning and Data Mining", "CPSC410 - Advanced Software Engineering", "CPSC404 - Advanced Relational Databases", "CPSC317 - Internet Computing","CPSC320 - Intermediate Algorithm Design and Analysis", "CPSC314 - Computer Graphics","CPSC322 - Introduction to Artificial Intelligence", "STAT300 - Intermediate Statistics for Applications"];
+
     $scope.test = "education";
 }]);
